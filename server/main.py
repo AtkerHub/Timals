caf41a8e22dcde694e44626bdf4727bc287d2f1c
AtkerHub <boczarkacper@gmail.com>
2026-09-12 12:27:27 +0200

    FastAPI: first test

--- server/main.py
+++ server/main.py
@@ -3,10 +3,23 @@ from sqlalchemy import text
 from sqlalchemy import MetaData
 from models import Base
 
+from fastapi import FastAPI
+
+
+
 engine = create_engine("postgresql+psycopg://admin:timmy@localhost:5433/timals-postgres-db", echo=True)
 
-with engine.connect() as connection:
-    result = connection.execute(text("select 1;"))
-    print(result.one())
+# - - - Connection test - - - 
+#with engine.connect() as connection:
+#    result = connection.execute(text("select 1;"))
+#    print(result.one())
 
 Base.metadata.create_all(engine)
+
+
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Timals says Hello~!"}
